ef8c86ba9acfbcf4bfbb5f675c647e3f9dcd5127
Emilio J A <facu@emilioja.com.ar>
2015-04-27 22:58:55 -0300

    Version original del Framework, pero con el ABObject.java y el ABType.java del DATALAB para aprovechar los metodos de deteccion de objetos.

---
+++ Angry Birds CMD.bat
@@ -0,0 +1,13 @@
+@echo off 
+cls
+echo.
+echo **************************
+echo *****-=[Angry Bird]=-*****
+echo **************************
+echo.
+echo inicio ABSoftware.jar
+D:
+cd d:\Users\Emilio J A\Documents\UTN - FRSF\Inteligencia Artificial 2015\Angry Birds\abV1.32
+echo.
+java -jar ABSoftware.jar -na 1
+pause
