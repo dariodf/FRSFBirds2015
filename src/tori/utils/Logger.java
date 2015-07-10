358261141526f4a4366d5b1a5566091e9ab29de5
Agus <agustin.pretto@gmail.com>
2015-07-10 11:11:13 -0300

    Fixes
    Mas Fixes Corregidos

--- src/tori/utils/Logger.java
+++ src/tori/utils/Logger.java
@@ -1,14 +1,20 @@
 package tori.utils;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public final class Logger {
-
+	public static String LogPath = "Output";
 	public static void Print(String Message) {
 		
 		try {
-			FileWriter fw = new FileWriter("Output/Log.txt", true);
+			File f = new File(LogPath);
+			if(!f.exists()){
+				f.mkdir();
+			}
+			
+			FileWriter fw = new FileWriter(LogPath + "/Log.txt", true);
 			fw.write(System.getProperty( "line.separator" ));
 			
 			fw.write(Message);
